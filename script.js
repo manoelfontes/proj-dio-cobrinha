499f6f258a32bf67a17e570474f7da32bfb00695
manoelfontes <mmessias2005@yahoo.com.br>
2021-08-05 16:12:15 -0300

    "criar comidinha"

--- script.js
+++ script.js
@@ -8,7 +8,10 @@ snake[0] = {
 }
 
 let direction = "right";
-
+let food = {
+    x: Math.floor(Math.random() * 15 + 1) * box,
+    y: Math.floor(Math.random() * 15 + 1) * box
+}
 function criarBG() {
     context.fillStyle = "lightgreen";
     context.fillRect(0, 0, 16 * box, 16 * box);
@@ -20,6 +23,10 @@ function criarCobrinha() {
     }
 }
 
+function drawFood(){
+    context.fillStyle = "red";
+    context.fillRect(food.x, food.y, box, box);
+}
 document.addEventListener('keydown',update);
 
 function update(event){
@@ -37,7 +44,8 @@ function inicarJogo(){
 
     criarBG();
     criarCobrinha();
-
+    drawFood();
+    
     let snakeX = snake[0].x;
     let snakeY = snake[0].y;
 
