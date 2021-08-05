fd4478d6173f630bb2b98cd5197e8133dbdb2057
manoelfontes <mmessias2005@yahoo.com.br>
2021-08-05 19:11:10 -0300

    "definir frutinha aleatoria e tamanho da combrina"

--- script.js
+++ script.js
@@ -38,14 +38,14 @@ function update(event){
 
 function inicarJogo(){
    if(snake[0].x > 15 * box && direction == "right") snake[0].x = 0;
-   if(snake[0].x < 0 * box && direction == "left") snake[0].x = 16 * box;
+   if(snake[0].x < 0 && direction == "left") snake[0].x = 16 * box;
    if(snake[0].y > 15 * box && direction == "down") snake[0].y = 0;
-   if(snake[0].y < 0 * box && direction == "up") snake[0].y = 16 * box;
+   if(snake[0].y < 0 && direction == "up") snake[0].y = 16 * box;
 
     criarBG();
     criarCobrinha();
     drawFood();
-    
+
     let snakeX = snake[0].x;
     let snakeY = snake[0].y;
 
@@ -54,10 +54,18 @@ function inicarJogo(){
     if(direction == "up") snakeY -= box;
     if(direction == "down") snakeY += box;
 
-    snake.pop();
+    if(snakeX != food.x || snakeY != food.y){
+        snake.pop();
+    }
+    else{
+         food.x = Math.floor(Math.random() * 15 + 1) * box;
+         food.y = Math.floor(Math.random() * 15 + 1) * box;
+    }
+
+
 
     let newHead = {
-        x: snakeX ,
+        x: snakeX,
         y: snakeY
     }
 
