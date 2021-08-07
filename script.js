1fdd49c3ab40b880fe1cd69178a7910c03867dc1
manoelfontes <mmessias2005@yahoo.com.br>
2021-08-07 18:58:30 -0300

    "algumas melhorias"

--- script.js
+++ script.js
@@ -1,33 +1,64 @@
+
 let canvas = document.getElementById("snake");
 let context = canvas.getContext("2d");
+let imgfruta = document.getElementById("img-fruta");
+let imgveneno = document.getElementById("img-veneno");
+var velocidade = 100;
+
 let box = 32;
 let snake = [];
 snake[0] = {
     x: 8 * box,
     y: 8 * box
 }
-
+let venenos = 0;
+let frutas = 0;
 let direction = "right";
 let food = {
     x: Math.floor(Math.random() * 15 + 1) * box,
     y: Math.floor(Math.random() * 15 + 1) * box
 }
+let veneno = {
+    x: Math.floor(Math.random() * 15 + 1) * box,
+    y: Math.floor(Math.random() * 15 + 1) * box
+}
 function criarBG() {
-    context.fillStyle = "lightgreen";
+    context.fillStyle = "black";
     context.fillRect(0, 0, 16 * box, 16 * box);
 }
 function criarCobrinha() {
-    for(i=0; i < snake.length; i++){
+    for(i=1; i < snake.length; i++){
         context.fillStyle = "green";
         context.fillRect(snake[i].x, snake[i].y, box, box);
+        
     }
 }
-
-function drawFood(){
+function cabecaCobrinha() {
+        context.fillStyle = "yellow";
+        context.fillRect(snake[0].x, snake[0].y, box, box);
+   }
+/* function drawFood(){
     context.fillStyle = "red";
     context.fillRect(food.x, food.y, box, box);
+    
+} */
+function drawFood(){
+    context.drawImage(imgfruta,food.x, food.y, box, box);
+    
 }
+
+/* function drawVeneno(){
+    context.fillStyle = "white";
+    context.fillRect(veneno.x, veneno.y, box, box);
+    
+} */
+function drawVeneno(){
+    context.drawImage(imgveneno,veneno.x, veneno.y, box, box);
+    
+}
+
 document.addEventListener('keydown',update);
+
 
 function update(event){
     if(event.keyCode == 37 && direction != "right") direction = "left";
@@ -36,7 +67,14 @@ function update(event){
     if(event.keyCode == 40 && direction != "up") direction = "down";
 }
 
-function inicarJogo(){
+function letreiro(){
+
+ document.getElementById("frutas").innerHTML ="<img id='img-fruta'src='fruta.jpg' width=32 height=32>" + frutas ;
+ document.getElementById("venenos").innerHTML = "<img id='img-fruta'src='veneno.png' width=32 height=32>"+ venenos;
+}
+
+function iniciarJogo(){
+
    if(snake[0].x > 15 * box && direction == "right") snake[0].x = 0;
    if(snake[0].x < 0 && direction == "left") snake[0].x = 16 * box;
    if(snake[0].y > 15 * box && direction == "down") snake[0].y = 0;
@@ -49,8 +87,10 @@ function inicarJogo(){
         }
     }
     criarBG();
+    cabecaCobrinha()
     criarCobrinha();
     drawFood();
+    drawVeneno();
 
     let snakeX = snake[0].x;
     let snakeY = snake[0].y;
@@ -60,15 +100,34 @@ function inicarJogo(){
     if(direction == "up") snakeY -= box;
     if(direction == "down") snakeY += box;
 
+    if(snakeX != veneno.x || snakeY != veneno.y) {
+   
+    }
+    else{
+        snake.pop();  
+        snake.pop();  
+        frutas -=1;
+        venenos +=1;
+        veneno.x = Math.floor(Math.random() * 15 + 1) * box;
+        veneno.y = Math.floor(Math.random() * 15 + 1) * box;
+        if(snake.length == 0){
+            clearInterval(jogo);
+            alert("Game Over :(");
+        }
+    }
+
+
     if(snakeX != food.x || snakeY != food.y){
-        snake.pop();
+        snake.pop();  
+  
     }
     else{
          food.x = Math.floor(Math.random() * 15 + 1) * box;
          food.y = Math.floor(Math.random() * 15 + 1) * box;
-    }
-
-
+         frutas +=1;
+         velocidade -=15;
+      
+}
 
     let newHead = {
         x: snakeX,
@@ -76,7 +135,11 @@ function inicarJogo(){
     }
 
     snake.unshift(newHead);
-}
+    letreiro();
 
-let jogo = setInterval(inicarJogo, 100);
+}
+   // iniciarJogo();
+
+    var jogo = setInterval(iniciarJogo,velocidade);
+
 
